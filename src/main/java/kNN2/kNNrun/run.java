9c5b3285d1c94f9020fdc2e393658cb04423b7e5
Your Name <you@example.com>
2016-12-10 19:05:08 -0500

    almost finish the mapper, but the mapper still need to transfer the data out

--- src/main/java/kNN2/kNNrun/run.java
+++ src/main/java/kNN2/kNNrun/run.java
@@ -5,7 +5,10 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
@@ -14,6 +17,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -59,61 +63,120 @@ public class run {
 	}
 	
 	
-	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, DoubleWritable>{
 
-		private final static IntWritable one = new IntWritable(1);
+		private final static DoubleWritable one = new DoubleWritable(0.000);
 		private Text word = new Text();
-		private IntWritable testSumWriteable;
 		private double testSum = 0.0000;
-		private int k = 10;
+		ArrayList<String> TestCaselist = new ArrayList<String>();
+		
+		private int k = 5;
+		TreeMap<String, Double> Kmap = new TreeMap<String, Double >();
+		
+		   public static String getAttemptId(Configuration conf) throws IllegalArgumentException
+		   {
+			   // This whole function used for chech the mapper ID
+		       if (conf == null) {
+		           throw new NullPointerException("conf is null");
+		       }
+
+		       String taskId = conf.get("mapred.task.id");
+		       if (taskId == null) {
+		           throw new IllegalArgumentException("Configutaion does not contain the property mapred.task.id");
+		       }
+		       //System.out.println(taskId);
+		       String[] parts = taskId.split("_");
+		       if (parts.length != 6 ||
+		               !parts[0].equals("attempt") ||
+		               (!"m".equals(parts[3]) && !"r".equals(parts[3]))) {
+		           throw new IllegalArgumentException("TaskAttemptId string : " + taskId + " is not properly formed");
+		       }
+
+		       return parts[4] + "-" + parts[5];
+		   }
 		
 		@Override
 		protected void setup(Context context) throws IOException, InterruptedException 
 		{
+			//System.out.println("Liang Xu in setup");
+			// suppose load the test date set
 			try
 			{
 				Path[] localFiles = DistributedCache.getLocalCacheFiles(context.getConfiguration());
+				//System.out.println("Local file is (Liang Xu)" + localFiles);
+				//System.out.println("Local file length is (Liang Xu)" + localFiles.length);
 				if(localFiles != null && localFiles.length > 0) {
-					System.out.println("Liang Xu");
+					String testFileString = FileUtils.readFileToString(new File(System.getProperty("user.dir")+"/test/smallTest1.arff"));
+					//System.out.println("Liang Xu in the mapper and the file length is " + testFileString.length());
+					StringTokenizer testValues = new StringTokenizer(testFileString, "\n");
+					String temp = null;
+					while (testValues.hasMoreTokens()) {
+						temp = testValues.nextToken();
+						TestCaselist.add(temp);
+					}
 				}
 
 			}catch(IOException ex) {
 				System.err.println("Exception in mapper setup: " + ex.getMessage());
 			}
-			
-			//String stringValue = FileUtils.readFileToString(new File("/home/liangxu/workspace/kNNHadoop/train"));
 		}
 		
-		
+		@Override
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			//StringTokenizer itr = new StringTokenizer(value.toString());
-			
-			StringTokenizer itr = new StringTokenizer(value.toString(),",");
-			String temp = null;
-			while (itr.hasMoreTokens()) {
-				temp = itr.nextToken();
-				testSum =testSum + Double.parseDouble(temp);
-				//System.out.println(word + " " + (int)testSum);
+			//System.out.println(getAttemptId(context.getConfiguration()));
+			/*
+			for (int i = 0; i < TestCaselist.size(); i++)
+			{
+				System.out.println(TestCaselist.get(i));
 			}
-			one.set( (int) testSum );
-			word.set(temp);
-			//testSumWriteable.set(testSum);
-			context.write(word, one);
+			*/
+			
+			for (int i = 0; i < TestCaselist.size(); i++)
+			{
+				StringTokenizer itr = new StringTokenizer(value.toString(),",");
+				StringTokenizer testitr = new StringTokenizer(TestCaselist.get(i).toString(),",");
+				String trainValueString = null;
+				double trainValueDouble = 0.00;
+				double testValueDouble = 0.00;
+				
+				
+				while (itr.hasMoreTokens()) 
+				{
+					trainValueString = itr.nextToken();
+					trainValueDouble = Double.parseDouble(trainValueString);
+					if(itr.hasMoreTokens())
+					{
+						//make sure this token is not the last one
+						testValueDouble =  Double.parseDouble(testitr.nextToken());
+						testSum =testSum + Math.pow(trainValueDouble-testValueDouble, 2);
+					}
+					//System.out.println(word + " " + (int)testSum);
+				}
+				//System.out.format("Distance between to the test is [ %10.1f ] -------->"+"[ " + trainValueString.toString() + " ] \n" ,testSum);
+				
+				one.set( testSum );
+				word.set(trainValueString.toString());
+				//testSumWriteable.set(testSum);
+				Kmap.put(trainValueString.toString(), testSum);
+				context.write(word, one);
+				testSum = 0.00;
+			}
 		}
 	}
 
-	public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+	public static class IntSumReducer extends Reducer<Text,DoubleWritable,Text,DoubleWritable> {
 		
-		private IntWritable result = new IntWritable();
+		private DoubleWritable result = new DoubleWritable();
 
-		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+		public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
 			
-			int sum = 0;
+			double sum = 0;
 			
-			for (IntWritable val : values) 
+			for (DoubleWritable val : values) 
 			{
 				//System.out.println("Liang Xu");
-				System.out.print(values);
+				//System.out.print(values);
 				sum += val.get();
 			}
 			
@@ -125,7 +188,7 @@ public class run {
 	
 	public static void main(String[] args) throws Exception 
 	{
-		/*
+		
 		System.out.println(args[0]);
 		if(args.length != 2)
 		{
@@ -134,6 +197,8 @@ public class run {
 		long startTime = System.currentTimeMillis();
 		
 		Instances data = null;
+		
+		/*
 		try{
 			BufferedReader reader = new BufferedReader(new FileReader(args[0]));
 			data = new Instances(reader);
@@ -145,18 +210,15 @@ public class run {
 		
 		predictions(data);
 
-		long endTime   = System.currentTimeMillis();
-		long totalTime = endTime - startTime;
-		System.out.println("total time mill second spend is " + totalTime);
-		System.out.print("LiangXu\n");
 		*/
 		
 		Configuration conf = new Configuration();
+		//JobConf job = new JobConf(conf,run.class);
 		Job job = Job.getInstance(conf, "word count");
 		job.setJarByClass(run.class);
 		
 		// Test data separation
-		DistributedCache.addCacheFile(new Path("/home/liangxu/workspace/kNNHadoop/train").toUri(),job.getConfiguration());
+		DistributedCache.addCacheFile(new Path(System.getProperty("user.dir")+"/test/smallTest1.arff").toUri(),job.getConfiguration());
 		
 		job.setMapperClass(TokenizerMapper.class);
 		job.setCombinerClass(IntSumReducer.class);
@@ -167,18 +229,19 @@ public class run {
 		
 		// Setup the Key Value type
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setOutputValueClass(DoubleWritable.class);
 		
 		// Input file and out put file foler
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
-		
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
-		if(job.isSuccessful()) {
-			System.out.println("Job was successful");
-		} else if(!job.isSuccessful()) {
-			System.out.println("Job was not successful");	
+
+		if(job.waitForCompletion(true))
+		{
+			long endTime   = System.currentTimeMillis();
+			long totalTime = endTime - startTime;
+			//totalTime = TimeUnit.MILLISECONDS.toSeconds(totalTime);
+			System.out.println("total time mill second spend is " + totalTime);
+			System.exit(1);
 		}
 	}
 
